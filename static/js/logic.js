280ba2b39c789ce927063c705ff69aa2c30f503b
bookwyrm513 <116978064+bookwyrm513@users.noreply.github.com>
2023-04-13 21:40:06 -0400

    fixing file location

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,5 @@
 // Store our API endpoint as queryURL
-var queryURL = "https://www.nps.gov/lib/npmap.js/4.0.0/examples/data/national-parks.geojson"
+var queryURL = "http://127.0.0.1:5000/np-geojson"
 
 // Perform a GET request to the query URL/
 d3.json(queryURL).then(function(data) {
@@ -29,13 +29,17 @@ function createFeatures(parkData) {
 function createMap(parks) {
 
   // Create the base layers.
-  var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  })
+  var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    subdomains: 'abcd',
+    minZoom: 1,
+    maxZoom: 16,
+    ext: 'jpg'
+  });
 
   // Create a baseMaps object.
   var baseMaps = {
-    "Street Map": street
+    "Street Map": Stamen_Watercolor 
   };
 
   // Create an overlay object to hold our overlay.
@@ -43,195 +47,44 @@ function createMap(parks) {
     Parks: parks
   };
 
+  var map = L.map('map').setView([39.0997, -94.578331], 12);
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+	maxZoom: 4,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+}).addTo(map);
+
+//   L.tileLayer('https://tile.openstreetmap.be/osmbe/{z}/{x}/{y}.png', {
+//      attribution:
+//          '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors' +
+//          ', Tiles courtesy of <a href="https://geo6.be/">GEO-6</a>',
+//      maxZoom: 18
+//  }).addTo(map);
+
   // Create our map, giving it the streetmap and earthquakes layers to display on load.
-  var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [street, parks]
-  });
+  // var myMap = L.map("my-custom-map", {
+  //   center: [
+  //     37.09, -95.71
+  //   ],
+  //   zoom: 5,
+  //   layers: [Stamen_Watercolor, parks]
+  // });
+
+  // var myMap = L.map('my-custom-map').setView([51.505, -0.09], 13);
+
+  // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
+  //     maxZoom: 18,
+  //     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+  //         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+  //         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  //     id: 'mapbox.streets'
+  // }).addTo(myMap);
 
   // Create a layer control.
   // Pass it our baseMaps and overlayMaps.
   // Add the layer control to the map.
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
+//   L.control.layers(baseMaps, overlayMaps, {
+//     collapsed: false
+//   }).addTo(map);
 
+// 
 }
-
-
-
-
-// // Store a URL in a constant variable 
-// const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
-
-// // Fetch the JSON data and console log it
-// d3.json(url).then(
-//   function(data) {
-//     console.log(data);
-//   });
- 
-// // Create a function that builds the bar chart
-// function buildBarChart(sample)
-// {
-//      // Use the D3 library to get all the sampels data
-//     d3.json(url).then((data) => {
-
-//         let sampleData = data.samples;
-
-//         // Filter the metadata 
-//         let sampleResult  = sampleData.filter(sampleResult => sampleResult.id == sample);
-
-//         // Get the data on the first sample
-//         let resultData = sampleResult[0];
-
-//         // Get the otu_ids, lables, and sample_values
-//         let otu_ids = resultData.otu_ids;
-//         let otu_labels = resultData.otu_labels;
-//         let sample_values = resultData.sample_values;
-
-//         // Slice top 10 to display in the chart 
-//         let yticks = otu_ids.slice(0, 10).map(id => `OTU ${id}` );
-//         let xValues = sample_values.slice(0, 10);
-//         let textLabels = otu_labels.slice(0, 10);
-
-//         // Create a bar chart 
-//         let barChart = {
-//             y: yticks.reverse(),
-//             x: xValues.reverse(),
-//             text: textLabels.reverse(),
-//             type: "bar",
-//             orientation: "h",
-//             mode: 'markers',
-//             marker: {
-//                 color: "#602060"
-//             }
-//         };
-
-//         let barData = [barChart]
-
-//         // Set a layout for the bar chart
-//         let layout = {
-//             title: "Top 10 Belly Button Bacteria"
-//         };
-
-//         // Plot the bar chart using Plotly
-//         Plotly.newPlot("bar", barData, layout)
-//     });
-// };
-
-// // Create a funtion that builds the bubble chart
-// function buildBubbleChart(sample)
-// {
-//      // Use the D3 library to get all the sampels data
-//      d3.json(url).then((data) => {
-
-//         let sampleData = data.samples;
-
-//         // Filter the metadata 
-//         let sampleResult = sampleData.filter(sampleResult => sampleResult.id == sample);
-
-//         // Get the data on the first sample
-//         let resultData = sampleResult[0];
-
-//         // Get the otu_ids, lables, and sample_values
-//         let otu_ids = resultData.otu_ids;
-//         let otu_labels = resultData.otu_labels;
-//         let sample_values = resultData.sample_values;
-
-//         // Set up a bubble chart
-//         let bubbleChart = {
-//             y: sample_values,
-//             x: otu_ids,
-//             text: otu_labels,
-//             mode: "markers",
-//             marker: {
-//                 size: sample_values,
-//                 color: otu_ids,
-//                 colorscale: "Electric"
-//             }
-//         };
-//         let bubbleData = [bubbleChart]
-
-//         // Set up a layout for the Bubble Chart
-//         let layout = {
-//             title: "Belly Button Bacteria Per Sample",
-//             hovermode: "closest",
-//             xaxis: {title: "OTU ID"}
-//         };
-
-//         // Plot the bar chart using Plotly
-//         Plotly.newPlot("bubble", bubbleData, layout)
-//     });
-// };
-
-// // Create a function that populates the demographics box
-// function buildDemographInfo(sample)
-// {
-//     // Use the D3 library to get all the sampels data
-//     d3.json(url).then((data) => {
-        
-//         // Get all the metadata
-//         let metaData = data.metadata;
-
-//         // Filter the metadata 
-//         let sampleResult = metaData.filter(sampleResult => sampleResult.id == sample);
-
-//         // Get the data on the first sample
-//         let resultData = sampleResult[0];
-//         // Clean the metadata (so that the previous sample data disappears once the new sample is selected)
-//         d3.select("#sample-metadata").html("");
-
-//         // Update the demographics box with keys and values from the sample data
-//         Object.entries(resultData).forEach(([key, value]) => {
-//             d3.select("#sample-metadata")
-//                 .append("h5").text(`${key}: ${value}`);
-//         });
-//     });
-// };
-
-// // Create a function that initializes the dashboard
-// function startDashboard()
-// {
-//     // Create a variable for the dropdown menu using the html id
-//     let select = d3.select("#selDataset");
-
-//     // Use the D3 library toget the sample names and populate the dropdown menu
-//     d3.json(url).then((data) => {
-//         let sampleNames = data.names;
-//         sampleNames.forEach((sample) => {
-//             select.append("option")
-//                 .text(sample)
-//                 .property("value", sample);
-//         });
-
-//         let sample1 = sampleNames[0];
-
-//         // Call the function to build the initial demographics box
-//         buildDemographInfo(sample1);
-
-//         // Call the function to build the initial bar chart
-//         buildBarChart(sample1);
-
-//         // Call the function to build the initial bubble chart
-//         buildBubbleChart(sample1);
-
-//       });    
-//     };
-
-//   // Create a function that updates the dashboard once another sample is selected 
-//   function optionChanged(item)
-//   {
-//     // Call the function to build the updated demographics box
-//     buildDemographInfo(item);
-//     // Call the function to build the updated bar chart
-//     buildBarChart(item);
-//      // Call the function to build the updated bubble chart
-//     buildBubbleChart(item);
-  
-//   }
-  
-// // Call the function to start the dashboard
-// startDashboard();
